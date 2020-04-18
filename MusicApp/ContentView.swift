61bb45c7429c1e28ed565043945ddc93c2843c42
perekrist <perekrist31@gmail.com>
2020-04-19 02:38:52 +0700

    Initial Commit

---
+++ MusicApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MusicApp
+//
+//  Created by Кристина Перегудова on 19.04.2020.
+//  Copyright © 2020 Кристина Перегудова. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

3e8d1c6011c9a827a117f47c1c3c8c80c5e80a62
perekrist <perekrist31@gmail.com>
2020-11-09 12:19:37 +0700

    add .gitignore

--- Pods/Target Support Files/Pods-MusicApp-MusicAppUITests/Pods-MusicApp-MusicAppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MusicApp_MusicAppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_MusicApp_MusicAppUITests
-@end
--- Pods/Target Support Files/Pods-MusicAppTests/Pods-MusicAppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MusicAppTests : NSObject
-@end
-@implementation PodsDummy_Pods_MusicAppTests
-@end
